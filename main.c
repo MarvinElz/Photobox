708496c0e247803809d76a9aaa004e978be01165
MarvinElz <MarvinElz@gmx.de>
2015-12-16 20:00:22 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 // TODO: include Boost or other internet-library http://www.boost.org/
 // TODO: try do reach http Webside
 // TODO: parse information
-// TODO: get Raspberry GPIO Running
+// TODO: get Raspberry GPIO Running http://raspberrypiguide.de/howtos/raspberry-pi-gpio-how-to/
+// http://www.element14.com/community/servlet/JiveServlet/previewBody/73950-102-4-309126/GPIO_Pi2.png?01AD=3GZgWMTpg1jDSMy2YO8hD1OqtjdCp9XqniBSBHoUhsn1I8J5raM6jOQ&01RI=B51CF70A37220D9&01NA=na
 // TODO: "Hello World" on screen
 // TODO: send information to screen
 // TODO: start script every 30 seconds
