53b81a548e68ec3821e7203d777170e4a641c649
MarvinElz <MarvinElz@gmx.de>
2015-12-16 19:58:51 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,10 @@
 // http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?hst=südhöhe
+
+
+// TODO: include Boost or other internet-library http://www.boost.org/
+// TODO: try do reach http Webside
+// TODO: parse information
+// TODO: get Raspberry GPIO Running
+// TODO: "Hello World" on screen
+// TODO: send information to screen
+// TODO: start script every 30 seconds
