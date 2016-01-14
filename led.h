1461ffe98ca621e748dedcd70c95771ee826a375
MarvinElz <MarvinElz@gmx.de>
2016-01-14 20:29:17 +0000

    Create led.h

---
+++ led.h
@@ -0,0 +1,14 @@
+#ifndef _LED_H_
+#define _LED_H_
+
+#define segA 26
+#define segB 22
+#define segC 25
+#define segD 27
+#define segE 28
+#define segF 24
+#define segG 23
+#define DP   29
+
+
+#endif
